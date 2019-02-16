6942a5c613307b6087c2deb6030de3c610f5cf7c
baidu <imguozhen@163.com>
2019-02-16 19:39:00 +0800

    generative_pytorch_version

--- generative_pytorch_version/run_test.sh
+++ generative_pytorch_version/run_test.sh
@@ -8,7 +8,6 @@
 #
 ######################################################################
 
-#pythonpath='/opt/compiler/gcc-4.8.2/lib/ld-linux-x86-64.so.2 --library-path /opt/compiler/gcc-4.8.2/lib /home/talk2/xuxinchao01/knowledge_tagging/krl-p40-local/pytorch1/bin/python'
 pythonpath='python'
 prefix=demo
 datapart=dev
@@ -27,13 +26,12 @@ fi
 
 ${pythonpath} ./tools/convert_conversation_corpus_to_model_text.py ${sample_file} ${text_file} ${topic_file} 1
 
-#${pythonpath} ./network.py --test --ckpt models/best.model --gen_file ./output/test.result --use_posterior False --gpu 0 > log.txt 2>&1
-${pythonpath} ./network.py --test --ckpt models/best.model --gen_file ./output/test.result --use_posterior False > log.txt 2>&1
-
+${pythonpath} ./network.py --test --ckpt models/best.model --gen_file ./output/test.result --use_posterior False --gpu 0 > log.txt 2>&1
 
 ${pythonpath} ./tools/topic_materialization.py ./output/test.result ./output/test.result.final ${topic_file}
 
-${pythonpath} ./tools/convert_result_for_eval.py ${sample_file} ./output/test.result.final ./output/test.result.eval
-
-${pythonpath} ./tools/eval.py ./output/test.result.eval
+if [ "${datapart}"x != "test"x ]; then
+    ${pythonpath} ./tools/convert_result_for_eval.py ${sample_file} ./output/test.result.final ./output/test.result.eval
+    ${pythonpath} ./tools/eval.py ./output/test.result.eval
+fi
 
